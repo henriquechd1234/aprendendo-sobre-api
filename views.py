888e9c29984689684359b8ec977af49853b3d01e
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2025-02-08 13:45:28 -0300

    api 2.0

--- views.py
+++ views.py
@@ -2,9 +2,10 @@ from main import app
 from flask import render_template
 import requests
 
-informaçoe = requests.get("https://api.deezer.com/album/302127")
-informaçoe = informaçoe.json()
 
 @app.route("/")
 def homepage():
+    api = "https://api.deezer.com/album/302127"
+    informaçoe = requests.get(api)
+    informaçoe = informaçoe.json()
     return render_template("index.html",informaçoe=informaçoe )

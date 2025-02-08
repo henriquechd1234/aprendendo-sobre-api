48a976343cf67b0180ce338323f1170e2a37f3ef
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2025-02-08 16:10:31 -0300

    slc

--- views.py
+++ views.py
@@ -1,11 +1,16 @@
 from main import app
 from flask import render_template
+from flask import jsonify
 import requests
 
 
 @app.route("/")
 def homepage():
-    api = "https://api.deezer.com/album/302127"
+    api = "https://api.deezer.com/album/"
     informaçoe = requests.get(api)
     info = informaçoe.json()
+
+
+
+
     return render_template("index.html",info=info)

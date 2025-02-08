f7af3306a3725d2e03947464a498c5bde43ea95f
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2025-02-08 11:56:28 -0300

    Tudo

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+import requests
+import json
+
+
+app = Flask(__name__)
+
+from views import * 
+
+informaçoe = requests.get("https://api.deezer.com/version/service/id/method/?parameters")
+
+
+
+if __name__ == "__main__":
+    app.run()

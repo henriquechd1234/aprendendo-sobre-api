bf9ee398479eb03c8f900f341c36723822e4ae40
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2025-02-08 13:49:54 -0300

    api 2.0

--- views.py
+++ views.py
@@ -7,5 +7,5 @@ import requests
 def homepage():
     api = "https://api.deezer.com/album/302127"
     informaçoe = requests.get(api)
-    informaçoe = informaçoe.json()
-    return render_template("index.html",informaçoe=informaçoe )
+    info = informaçoe.json()
+    return render_template("index.html",info)

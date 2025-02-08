a56a046d1840404ac524146aa066c6a23edc44dc
henriquechd1234 <159437428+henriquechd1234@users.noreply.github.com>
2025-02-08 13:41:34 -0300

    apis deezer

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from main import app
 from flask import render_template
 import requests
 
-informaçoe = requests.get("https://api.deezer.com/version/service/id/method/?parameters")
+informaçoe = requests.get("https://api.deezer.com/album/302127")
 informaçoe = informaçoe.json()
 
 @app.route("/")
